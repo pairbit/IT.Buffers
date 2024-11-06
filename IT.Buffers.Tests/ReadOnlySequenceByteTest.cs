e15a16189635e080e73233d5ed7b4a31334d694d
Ivan Tikhonov <itikhonov@mail.com>
2024-11-06 20:19:28 +0300

    tests

---
+++ IT.Buffers.Tests/ReadOnlySequenceByteTest.cs
@@ -0,0 +1,17 @@
+﻿namespace IT.Buffers.Tests;
+
+public class ReadOnlySequenceByteTest : ReadOnlySequenceTest<byte>
+{
+    private ReadOnlyMemory<byte>[] Data = [
+        "1 segment"u8.ToArray(),
+        "2 segment"u8.ToArray(),
+        "3 segment"u8.ToArray()
+        ];
+
+    protected override IEnumerable<ReadOnlyMemory<byte>> GetMemories() => Data;
+
+    protected override void ReadMemory(ReadOnlyMemory<byte> memory, int index)
+    {
+        Assert.That(Data[index].Span.SequenceEqual(memory.Span), Is.True);
+    }
+}
