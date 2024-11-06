843e0196106bc686182314c2d042a722d63e14c3
Ivan Tikhonov <itikhonov@mail.com>
2024-11-06 19:35:20 +0300

    add ReadOnlySequenceBuilderPool

---
+++ IT.Buffers/ReadOnlySequenceBuilderPool.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace IT.Buffers;
+
+public class ReadOnlySequenceBuilderPool<T>
+{
+    private static readonly ConcurrentQueue<ReadOnlySequenceBuilder<T>> _queue = new();
+
+    public static ReadOnlySequenceBuilder<T> Rent()
+    {
+        if (_queue.TryDequeue(out var builder)) return builder;
+
+        return new ReadOnlySequenceBuilder<T>();
+    }
+
+    public static void Return(ReadOnlySequenceBuilder<T> builder)
+    {
+        if (builder == null) throw new ArgumentNullException(nameof(builder));
+
+        builder.Reset();
+        _queue.Enqueue(builder);
+    }
+}
