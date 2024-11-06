971450967d30f067c50a20c74aa08a3be4c8b943
Ivan Tikhonov <itikhonov@mail.com>
2024-11-06 20:12:36 +0300

    test

---
+++ IT.Buffers.Tests/ReadOnlySequenceCharTest.cs
@@ -0,0 +1,17 @@
+﻿
+namespace IT.Buffers.Tests;
+
+public class ReadOnlySequenceCharTest : ReadOnlySequenceTest<char>
+{
+    protected override IEnumerable<ReadOnlyMemory<char>> GetMemories()
+    {
+        yield return "1 segment".ToCharArray();
+        yield return "2 segment".ToCharArray();
+        yield return "3 segment".ToCharArray();
+    }
+
+    protected override void ReadMemory(ReadOnlyMemory<char> memory, SequencePosition position)
+    {
+
+    }
+}
--- IT.Buffers.Tests/ReadOnlySequenceTest.cs
+++ IT.Buffers.Tests/ReadOnlySequenceTest.cs
@@ -1,10 +1,46 @@
-﻿namespace IT.Buffers.Tests;
+﻿using System.Buffers;
 
-public class ReadOnlySequenceTest
+namespace IT.Buffers.Tests;
+
+public abstract class ReadOnlySequenceTest<T>
 {
     [Test]
     public void Test()
     {
-
+        var builder = ReadOnlySequenceBuilderPool<T>.Rent();
+        
+		try
+		{
+            Builder(builder);
+        }
+		finally
+		{
+            ReadOnlySequenceBuilderPool<T>.Return(builder);
+        }
     }
+
+    private void Builder(ReadOnlySequenceBuilder<T> builder)
+    {
+        foreach (var memory in GetMemories())
+        {
+            builder.Add(memory);
+        }
+
+        Sequence(builder.Build());
+    }
+
+    private void Sequence(ReadOnlySequence<T> sequence)
+    {
+        var position = sequence.Start;
+        while (sequence.TryGet(ref position, out var memory))
+        {
+            ReadMemory(memory, position);
+
+            if (position.GetObject() == null) break;
+        }
+    }
+
+    protected abstract IEnumerable<ReadOnlyMemory<T>> GetMemories();
+
+    protected abstract void ReadMemory(ReadOnlyMemory<T> memory, SequencePosition position);
 }
