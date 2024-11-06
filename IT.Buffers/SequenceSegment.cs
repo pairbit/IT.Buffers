459953bf1e1a9fbfbb2b4ede844ded7456f87d15
Ivan Tikhonov <itikhonov@mail.com>
2024-11-06 18:03:27 +0300

    add SequenceSegment

---
+++ IT.Buffers/SequenceSegment.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Buffers;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace IT.Buffers;
+
+public class SequenceSegment<T> : ReadOnlySequenceSegment<T>
+{
+    private bool _returnToPool;
+
+    public void SetBuffer(ReadOnlyMemory<T> memory, bool returnToPool)
+    {
+        Memory = memory;
+        _returnToPool = returnToPool;
+    }
+
+    public void SetRunningIndexAndNext(long runningIndex, SequenceSegment<T>? next)
+    {
+        RunningIndex = runningIndex;
+        Next = next;
+    }
+
+    public void Reset()
+    {
+        if (_returnToPool)
+        {
+            if (MemoryMarshal.TryGetArray(Memory, out var segment))
+            {
+                var array = segment.Array;
+                if (array != null) ArrayPool<T>.Shared.Return(array,
+                    clearArray: RuntimeHelpers.IsReferenceOrContainsReferences<T>());
+            }
+        }
+        Memory = default;
+        RunningIndex = 0;
+        Next = null;
+    }
+}
