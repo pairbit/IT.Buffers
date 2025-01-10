d6ca85ae6672c7d1ee0907542ca4b99dbc3107e0
Ivan Tikhonov <itikhonov@mail.com>
2025-01-10 20:51:47 +0300

    tests

--- IT.Buffers.Tests/LinkedBufferWriterTest.cs
+++ IT.Buffers.Tests/LinkedBufferWriterTest.cs
@@ -5,8 +5,20 @@ public class LinkedBufferWriterTest
     [Test]
     public void Test()
     {
-        var writer = new LinkedBufferWriter<byte>();
-        
+        var writer = LinkedBufferWriterPool<byte>.Rent(1);
 
+        var span = writer.GetSpan();
+
+        Random.Shared.NextBytes(span);
+
+        writer.Advance(span.Length);
+
+        span = writer.GetSpan();
+
+        Random.Shared.NextBytes(span);
+
+        writer.Advance(span.Length);
+
+        LinkedBufferWriterPool<byte>.Return(writer);
     }
 }
