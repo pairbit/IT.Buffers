a3e75370ffcc38bab31bea958461fe40e952d1b5
Ivan Tikhonov <itikhonov@mail.com>
2025-01-13 21:37:27 +0300

    add tests

--- IT.Buffers.Tests/ValueBufferWriterTest.cs
+++ IT.Buffers.Tests/ValueBufferWriterTest.cs
@@ -1,4 +1,4 @@
-﻿using System.Buffers;
+﻿using IT.Buffers.Interfaces;
 
 namespace IT.Buffers.Tests;
 
@@ -21,10 +21,10 @@ public class ValueBufferWriterTest
     }
 
     private static void Test<TBufferWriter>(ref TBufferWriter writer)
-        where TBufferWriter : IBufferWriter<byte>
+        where TBufferWriter : IAdvancedBufferWriter<byte>
     {
-        //Assert.That(writer.Written, Is.EqualTo(0));
-        //Assert.That(writer.WrittenLong, Is.EqualTo(0));
+        Assert.That(writer.Written, Is.EqualTo(0));
+        Assert.That(writer.WrittenLong, Is.EqualTo(0));
 
         var span = writer.GetSpan();
 
@@ -32,7 +32,27 @@ public class ValueBufferWriterTest
 
         writer.Advance(span.Length);
 
-        //Assert.That(writer.Written, Is.EqualTo(span.Length));
-        //Assert.That(writer.WrittenLong, Is.EqualTo(span.Length));
+        Assert.That(writer.Written, Is.EqualTo(span.Length));
+        Assert.That(writer.WrittenLong, Is.EqualTo(span.Length));
+
+        var bytes = new byte[writer.Written];
+
+        Assert.That(writer.TryWrite(bytes), Is.True);
+        Assert.That(span.SequenceEqual(bytes), Is.True);
+
+        bytes.AsSpan().Clear();
+        Assert.That(span.SequenceEqual(bytes), Is.False);
+
+        var fixedBuffer = new ValueFixedBufferWriter<byte>(bytes);
+
+        Assert.That(fixedBuffer.Capacity, Is.EqualTo(bytes.Length));
+        Assert.That(fixedBuffer.FreeCapacity, Is.EqualTo(bytes.Length));
+        Assert.That(fixedBuffer.Written, Is.EqualTo(0));
+
+        writer.Write(ref fixedBuffer);
+
+        Assert.That(fixedBuffer.Written, Is.EqualTo(bytes.Length));
+
+        Assert.That(span.SequenceEqual(bytes), Is.True);
     }
 }
