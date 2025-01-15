2f84d810c34de4b6dc62177946be38fa639cbc16
Ivan Tikhonov <itikhonov@mail.com>
2025-01-15 19:18:50 +0300

    add BufferSizeTest

---
+++ IT.Buffers.Tests/BufferSizeTest.cs
@@ -0,0 +1,13 @@
+﻿namespace IT.Buffers.Tests;
+
+public class BufferSizeTest
+{
+    [Test]
+    public void Test()
+    {
+        Assert.That(BufferSize<char>.KB_256, Is.EqualTo(BufferSize.KB_128));
+        Assert.That(BufferSize<int>.KB_256, Is.EqualTo(BufferSize.KB_64));
+        Assert.That(BufferSize<long>.KB_256, Is.EqualTo(BufferSize.KB_32));
+        Assert.That(BufferSize<Guid>.KB_256, Is.EqualTo(BufferSize.KB_16));
+    }
+}
