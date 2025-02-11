97073ddcc7fe66d81a153c751ea99f97cba259fb
Ivan Tikhonov <itikhonov@mail.com>
2025-02-11 13:21:50 +0300

    add IBufferReader

---
+++ IT.Buffers/Interfaces/IBufferReader`1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IT.Buffers.Interfaces;
+
+internal interface IBufferReader<T>
+{
+    void Advance(int count);
+
+    ReadOnlyMemory<T> GetMemory(int sizeHint = 0);
+
+    ReadOnlySpan<T> GetSpan(int sizeHint = 0);
+}
