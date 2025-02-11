b2519b255b0735e94b2ae44be3f09619a7a9d90b
Ivan Tikhonov <itikhonov@mail.com>
2025-02-11 13:53:20 +0300

    BufferWriters

---
+++ IT.Buffers/BufferWriters.cs
@@ -0,0 +1,12 @@
+﻿using System.Buffers;
+
+namespace IT.Buffers;
+
+//BufferWriterFactory??
+internal class BufferWriters
+{
+    public IBufferWriter<T> GetBufferWriter<T>()
+    {
+        throw new System.NotImplementedException();
+    }
+}
