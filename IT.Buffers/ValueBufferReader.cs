c137b9d8de6b0a0ac25f4a63e839c39ee5b4149f
Ivan Tikhonov <itikhonov@mail.com>
2025-02-11 13:27:39 +0300

    add ValueBufferReader

---
+++ IT.Buffers/ValueBufferReader.cs
@@ -0,0 +1,50 @@
+﻿using IT.Buffers.Interfaces;
+using System;
+using System.Buffers;
+
+namespace IT.Buffers;
+
+internal ref struct ValueBufferReader<T> : IBufferReader<T>
+{
+    private ReadOnlySequence<T> _sequence;
+    private ReadOnlyMemory<T> _memory;
+    private ReadOnlySpan<T> _span;
+
+    public int Consumed { get; }
+
+    public long ConsumedLong { get; }
+
+    public bool HasMemory { get; }
+
+    public bool HasSequence { get; }
+
+    public ValueBufferReader(in ReadOnlySequence<T> sequence)
+    {
+        _sequence = sequence;
+    }
+
+    public ValueBufferReader(ReadOnlyMemory<T> memory)
+    {
+        _memory = memory;
+    }
+
+    public ValueBufferReader(ReadOnlySpan<T> span)
+    {
+        _span = span;
+    }
+
+    public void Advance(int count)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ReadOnlyMemory<T> GetMemory(int sizeHint = 0)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ReadOnlySpan<T> GetSpan(int sizeHint = 0)
+    {
+        throw new NotImplementedException();
+    }
+}
