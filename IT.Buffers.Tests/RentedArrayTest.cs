52a656f8cc373cc7fa9b72af36a99e2023256015
Ivan Tikhonov <itikhonov@mail.com>
2025-11-14 15:48:37 +0300

    tests

--- IT.Buffers.Tests/RentedArrayTest.cs
+++ IT.Buffers.Tests/RentedArrayTest.cs
@@ -5,6 +5,39 @@ internal class RentedArrayTest
     [Test]
     public void Test()
     {
+        var array = RentedArray<byte>.Empty;
 
+        Assert.That(array.Equals(default), Is.False);
+        Assert.That(array.Equals(RentedArray<byte>.Empty), Is.True);
+
+        Equals(array);
+
+        var shared = new RentedArray<byte>([], RentedArrayType.Shared);
+        Equals(shared, type: RentedArrayType.Shared);
+
+        var global = new RentedArray<byte>([], RentedArrayType.Global);
+        Equals(global, type: RentedArrayType.Global);
+
+        var external = new RentedArray<byte>([], RentedArrayType.External);
+        Equals(external, type: RentedArrayType.External);
+
+        Assert.That(array.Equals(shared), Is.False);
+        Assert.That(array.Equals(global), Is.False);
+        Assert.That(array.Equals(external), Is.False);
+
+        Assert.That(shared.Equals(global), Is.False);
+        Assert.That(shared.Equals(external), Is.False);
+
+        Assert.That(global.Equals(external), Is.False);
+    }
+
+    private static void Equals(RentedArray<byte> array,
+        int offset = 0, int count = 0,
+        RentedArrayType type = RentedArrayType.None)
+    {
+        Assert.That(array.Array != null && array.Array.Length == 0, Is.True);
+        Assert.That(array.Offset, Is.EqualTo(offset));
+        Assert.That(array.Count, Is.EqualTo(count));
+        Assert.That(array.Type, Is.EqualTo(type));
     }
 }
