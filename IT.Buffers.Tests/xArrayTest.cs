d839e813f8220810b26e275bdbf4e768cebe3e8b
Ivan Tikhonov <itikhonov@mail.com>
2026-05-23 20:31:07 +0300

    BoundedArrayPool

---
+++ IT.Buffers.Tests/xArrayTest.cs
@@ -0,0 +1,32 @@
+﻿using IT.Buffers.Internal;
+
+namespace IT.Buffers.Tests;
+
+internal class xArrayTest
+{
+    [Test]
+    public void SelectBucketIndexTest()
+    {
+        Assert.That(xArray.SelectBucketIndex(1), Is.EqualTo(0));
+        Assert.That(xArray.SelectBucketIndex(15), Is.EqualTo(0));
+        Assert.That(xArray.SelectBucketIndex(16), Is.EqualTo(0));
+        Assert.That(xArray.SelectBucketIndex(17), Is.EqualTo(1));
+
+        Assert.That(xArray.SelectBucketIndex(1024), Is.EqualTo(6));
+        Assert.That(xArray.SelectBucketIndex(int.MaxValue), Is.EqualTo(27));
+        Assert.That(xArray.SelectBucketIndex(BufferSize.Max), Is.EqualTo(27));
+       
+        Assert.That(xArray.SelectBucketIndex(0), Is.EqualTo(28));
+        Assert.That(xArray.SelectBucketIndex(-1), Is.EqualTo(28));
+    }
+
+    [Test]
+    public void GetMaxSizeForBucketTest()
+    {
+        Assert.That(xArray.GetMaxSizeForBucket(0), Is.EqualTo(16));
+        Assert.That(xArray.GetMaxSizeForBucket(1), Is.EqualTo(32));
+        Assert.That(xArray.GetMaxSizeForBucket(2), Is.EqualTo(64));
+        Assert.That(xArray.GetMaxSizeForBucket(3), Is.EqualTo(128));
+        Assert.That(xArray.GetMaxSizeForBucket(26), Is.EqualTo(1073741824));
+    }
+}
--- IT.Buffers/ArrayPools/BoundedArrayPool.cs
+++ IT.Buffers/ArrayPools/BoundedArrayPool.cs
@@ -1,41 +1,60 @@
-﻿using System;
+﻿using IT.Buffers.Internal;
+using System;
 using System.Buffers;
+using System.Diagnostics;
 
-/*
 namespace IT.Buffers;
 
-public class BoundedArrayPool<T> : ArrayPool<T>
+internal class BoundedArrayPool<T> : ArrayPool<T>
 {
-    private readonly BoundedConcurrentQueue<T[]> _queue;
+    private const int NumBuckets = 27;
+    private readonly BoundedConcurrentQueue<T[]>?[] _buckets = new BoundedConcurrentQueue<T[]>[NumBuckets];
 
-    public BoundedArrayPool(int power2)
+    public BoundedArrayPool()
     {
-        _queue = new(power2);
+        //_queue = new(power2);
     }
-
-    public RentedArray<T> RentArray(int minimumLength)
-    {
-        //_queue.TryDequeue(out array)
-
-        throw new NotImplementedException();
-    }
-
-    public bool TryReturn(T[] array, bool clearArray = false)
-    {
-        return _queue.TryEnqueue(array);
-    }
-
-    #region ArrayPool
 
     public override T[] Rent(int minimumLength)
     {
-        throw new NotImplementedException();
+        int bucketIndex = xArray.SelectBucketIndex(minimumLength);
+
+        var buckets = _buckets;
+        if ((uint)bucketIndex < (uint)buckets.Length)
+        {
+            var bucket = buckets[bucketIndex];
+            if (bucket is not null)
+            {
+                if (bucket.TryDequeue(out var buffer))
+                {
+                    Debug.Assert(buffer.Length >= minimumLength);
+                    return buffer;
+                }
+            }
+
+            // No buffer available.  Ensure the length we'll allocate matches that of a bucket
+            // so we can later return it.
+            minimumLength = xArray.GetMaxSizeForBucket(bucketIndex);
+        }
+        else if (minimumLength == 0)
+        {
+            return [];
+        }
+        else if (minimumLength < 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(minimumLength));
+        }
+
+        return xArray.AllocateUninitialized<T>(minimumLength);
     }
 
     public override void Return(T[] array, bool clearArray = false)
     {
+        if (array == null)
+            throw new ArgumentNullException(nameof(array));
+
+        int bucketIndex = xArray.SelectBucketIndex(array.Length);
+
         throw new NotImplementedException();
     }
-
-    #endregion ArrayPool
-}*/
+}
