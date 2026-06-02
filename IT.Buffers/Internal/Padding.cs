55d55fe83718025b57c9106636b7a443586db42e
ITikhonov <ITikhonov@lanit.ru>
2026-06-02 18:03:13 +0300

    Padding fixed 64 size

--- IT.Buffers/Internal/Padding.cs
+++ IT.Buffers/Internal/Padding.cs
@@ -3,9 +3,12 @@
 //https://github.com/dotnet/runtime/blob/main/src/libraries/System.Private.CoreLib/src/Internal/Padding.cs
 internal static class Padding
 {
+    /*
 #if TARGET_ARM64 || TARGET_LOONGARCH64
     internal const int CACHE_LINE_SIZE = 128;
 #else
     internal const int CACHE_LINE_SIZE = 64;
 #endif
+    */
+    internal const int CACHE_LINE_SIZE = 64;
 }
