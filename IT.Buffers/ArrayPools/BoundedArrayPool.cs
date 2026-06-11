464636513f6f6b69898b5acda84279c7f929ca40
ITikhonov <ITikhonov@lanit.ru>
2026-06-11 10:34:03 +0300

    ArrayBucket

--- IT.Buffers/ArrayPools/BoundedArrayPool.cs
+++ IT.Buffers/ArrayPools/BoundedArrayPool.cs
@@ -2,18 +2,15 @@
 using System;
 using System.Buffers;
 using System.Diagnostics;
-using System.Diagnostics.CodeAnalysis;
-using System.Threading;
 
 namespace IT.Buffers;
 
 public class BoundedArrayPool<T> : ArrayPool<T>
 {
     private const int LastBucketIndex = 27;
-    private static readonly object _empty = new();
 
-    private readonly Bucket[] _buckets;
-    private readonly Bucket? _lastBucket;
+    private readonly ArrayBucket<T>[] _buckets;
+    private readonly ArrayBucket<T>? _lastBucket;
 
     public BoundedArrayPool(BoundedArrayPoolOptions options)
     {
@@ -22,12 +19,12 @@ public class BoundedArrayPool<T> : ArrayPool<T>
         if (length == BoundedArrayPoolOptions.MaxLength)
         {
             length--;
-            _lastBucket = new Bucket(LastBucketIndex, pow2s[LastBucketIndex]);
+            _lastBucket = new ArrayBucket<T>(LastBucketIndex, pow2s[LastBucketIndex]);
         }
-        var buckets = new Bucket[length];
+        var buckets = new ArrayBucket<T>[length];
         for (int i = 0; i < buckets.Length; i++)
         {
-            buckets[i] = new Bucket(i, pow2s[i]);
+            buckets[i] = new ArrayBucket<T>(i, pow2s[i]);
         }
         _buckets = buckets;
     }
@@ -121,106 +118,6 @@ public class BoundedArrayPool<T> : ArrayPool<T>
         for (int i = buckets.Length - 1; i >= 0; i--)
         {
             buckets[i].Clear();
-        }
-    }
-
-    class Bucket
-    {
-        public readonly int _length;
-        public readonly int _pow2;
-        public object? _obj;
-
-        public BoundedConcurrentQueue<T[]>? Queue => _obj as BoundedConcurrentQueue<T[]>;
-
-        public T[]? Array => _obj as T[];
-
-        public Bucket(int index, int pow2)
-        {
-            _length = xArray.GetMaxSizeForBucket(index);
-            _pow2 = pow2;
-
-            if (pow2 == 0)
-                _obj = _empty;
-        }
-
-        public bool TryEnqueue(T[] array)
-        {
-            Debug.Assert(array != null);
-
-            if (array.Length != _length)
-                throw new ArgumentException("Buffer not from pool.", nameof(array));
-
-            var obj = _obj ?? CreateQueue();
-            if (obj is BoundedConcurrentQueue<T[]> queue)
-            {
-                return queue.TryEnqueue(array);
-            }
-
-            if (ReferenceEquals(obj, _empty))
-                return Interlocked.CompareExchange(ref _obj, array, _empty) == _empty;
-
-            return false;
-        }
-
-        public bool TryDequeue([MaybeNullWhen(false)] out T[] array)
-        {
-            var obj = _obj;
-            if (obj is BoundedConcurrentQueue<T[]> queue)
-            {
-                if (queue.TryDequeue(out array))
-                {
-                    Debug.Assert(array.Length == _length);
-
-                    return true;
-                }
-            }
-            else if (obj is T[] buffer)
-            {
-                do
-                {
-                    Debug.Assert(buffer.Length == _length);
-
-                    var value = Interlocked.CompareExchange(ref _obj, _empty, buffer);
-                    if (ReferenceEquals(value, buffer))
-                    {
-                        array = buffer;
-                        return true;
-                    }
-
-                    if (value is T[] otherBuffer)
-                    {
-                        buffer = otherBuffer;
-                    }
-                    else
-                    {
-                        break;
-                    }
-                } while (true);
-            }
-
-            array = default;
-            return false;
-        }
-
-        public void Clear()
-        {
-            var obj = _obj;
-            if (obj is BoundedConcurrentQueue<T[]>)
-            {
-                _obj = null;
-            }
-            else if (obj is T[] array)
-            {
-                Debug.Assert(array.Length == _length);
-
-                _obj = _empty;
-            }
-        }
-
-        private object CreateQueue()
-        {
-            var queue = new BoundedConcurrentQueue<T[]>(_pow2);
-            return Interlocked.CompareExchange(ref _obj, queue, null) ?? queue;
         }
     }
 }
