a019eec9c56fef3a809ce8412f5050f636f8f68e
ITikhonov <ITikhonov@lanit.ru>
2026-06-12 22:24:00 +0300

    SharedArrayPoolTest

--- IT.Buffers.Tests/SharedArrayPoolTest.cs
+++ IT.Buffers.Tests/SharedArrayPoolTest.cs
@@ -6,10 +6,17 @@ namespace IT.Buffers.Tests;
 
 internal class SharedArrayPoolTest
 {
-    [Test]
+    //[Test]
     public void Test()
     {
-        //var shared = ArrayPool<byte>.Shared;
+        var shared = ArrayPool<byte>.Shared;
+
+        var r1 = shared.Rent(8 * 1024);
+        var r2 = shared.Rent(8 * 1024);
+
+        shared.Return(r1);
+        shared.Return(r2);
+
 
 
         Assert.That(SharedArrayPool<byte>.TrimCallbackCreated, Is.True);
@@ -23,6 +30,31 @@ internal class SharedArrayPoolTest
 
         [UnsafeAccessor(UnsafeAccessorKind.Field, Name = "_trimCallbackCreated")]
         private static extern ref bool get_trimCallbackCreated([UnsafeAccessorType(TypeName)] object pool);
+    }
+
+    static class SharedArrayPoolStatics
+    {
+        internal static readonly int s_partitionCount = GetPartitionCount();
+
+        internal static readonly int s_maxArraysPerPartition = GetMaxArraysPerPartition();
+
+        internal static int GetPartitionIndex() => (int)((uint)Thread.GetCurrentProcessorId() % (uint)s_partitionCount);
+
+        private static int GetPartitionCount()
+        {
+            int partitionCount = int.TryParse("DOTNET_SYSTEM_BUFFERS_SHAREDARRAYPOOL_MAXPARTITIONCOUNT", out int result) && result > 0 ?
+                result :
+                int.MaxValue;
+
+            return Math.Min(partitionCount, Environment.ProcessorCount);
+        }
+
+        private static int GetMaxArraysPerPartition()
+        {
+            return int.TryParse("DOTNET_SYSTEM_BUFFERS_SHAREDARRAYPOOL_MAXARRAYSPERPARTITION", out int result) && result > 0 ?
+                result :
+                32;
+        }
     }
 }
 #endif
