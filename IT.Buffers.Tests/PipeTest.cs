b0a97fe51d7da64c3c44070b070ad1a7e48f1562
ITikhonov <ITikhonov@lanit.ru>
2026-06-15 19:27:03 +0300

    tests

---
+++ IT.Buffers.Tests/PipeTest.cs
@@ -0,0 +1,34 @@
+﻿#if NET10_0_OR_GREATER
+using IT.Buffers.Extensions;
+using System.Buffers;
+using System.IO.Pipelines;
+
+namespace IT.Buffers.Tests;
+
+internal class PipeTest
+{
+    //[Test]
+    public async Task Test()
+    {
+        var hybridPool = new HybridArrayPool<byte>(HybridArrayPoolOptions.Create());
+        var hybridMemoryPool = BufferPool.CreateMemoryPool(hybridPool);
+
+        var pipeOptions = new PipeOptions(hybridMemoryPool);
+
+        var pipe = new Pipe(pipeOptions);
+        var writer = pipe.Writer;
+
+        var bytes = new byte[BufferSize.MB];
+        Random.Shared.NextBytes(bytes);
+        var stream = new MemoryStream(bytes);
+
+        await writer.WriteAsync(stream);
+
+        //await writer.FlushAsync();
+
+        await writer.CompleteAsync();
+
+        pipe.Reset();
+    }
+}
+#endif
