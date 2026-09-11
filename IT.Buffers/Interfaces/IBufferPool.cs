53e2f4eea25243f9b059284d64b356028d01f307
Ivan Tikhonov <pairbit@gmail.com>
2026-09-11 20:41:12 +0300

    fix

--- IT.Buffers/Interfaces/IBufferPool.cs
+++ IT.Buffers/Interfaces/IBufferPool.cs
@@ -2,13 +2,19 @@
 
 namespace IT.Buffers;
 
+//Интерфейс поможет сделать общее хранилище всех пулов.
+//для очистки всех пулов и контроля памяти
 public interface IBufferPool
 {
     int Id { get; }
 
     Type BufferType { get; }
 
-    //bool IsBounded { get; }
+    //bool IsBounded => BoundedLimit > 0;
 
     //int BoundedLimit { get; }
+
+    //void Clear();
+
+    //void Trim(int limit);
 }
