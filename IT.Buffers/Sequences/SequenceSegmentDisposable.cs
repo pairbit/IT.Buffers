9f328fec48fcdcbd52b27bc1151ee9e2e6ecb3e0
Ivan Tikhonov <pairbit@gmail.com>
2026-09-16 00:05:23 +0300

    SequenceSegmentDisposable

---
+++ IT.Buffers/Sequences/SequenceSegmentDisposable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Buffers;
+
+namespace IT.Buffers;
+
+internal abstract class SequenceSegmentDisposable<T> : SequenceSegment<T>, IMemoryOwner<T>
+{
+    public void Dispose()
+    {
+        throw new NotImplementedException();
+    }
+}
