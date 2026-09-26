69964643e98cb8a7b964881cf552943364f75642
Ivan Tikhonov <pairbit@gmail.com>
2026-09-26 10:01:32 +0300

    extension

---
+++ IT.Buffers/Extensions/xBase64.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics;
+
+namespace System.Buffers.Text;
+
+internal static class xBase64
+{
+    extension(Base64)
+    {
+        public static long EncodeToUtf8(ReadOnlySpan<byte> bytes, IBufferWriter<byte> bufferWriter)
+        {
+            if (bufferWriter == null) throw new ArgumentNullException(nameof(bufferWriter));
+
+            long lengthLong = 0;
+
+            while (bytes.Length > 0)
+            {
+                var status = Base64.EncodeToUtf8(bytes, bufferWriter.GetSpan(4), out var consumed, out var written);
+
+                if (written > 0)
+                {
+                    bufferWriter.Advance(written);
+
+                    lengthLong += written;
+
+                    bytes = bytes.Slice(consumed);
+                }
+                else if (status == OperationStatus.DestinationTooSmall)
+                {
+                    throw new InvalidOperationException("DestinationTooSmall");
+                }
+                else if (status == OperationStatus.DestinationTooSmall)
+                {
+                    throw new InvalidOperationException("DestinationTooSmall");
+                }
+
+                if (status == OperationStatus.Done)
+                    break;
+
+                Debug.Assert(status == OperationStatus.DestinationTooSmall);
+            }
+
+            return lengthLong;
+        }
+    }
+}
---
+++ IT.Buffers/Extensions/xTextEncoder.cs
@@ -0,0 +1,36 @@
+﻿#if NET
+using System.Buffers;
+using System.Diagnostics;
+
+namespace System.Text.Encodings.Web;
+
+internal static class xTextEncoder
+{
+    public static long EncodeUtf8(this TextEncoder textEncoder, ReadOnlySpan<byte> utf8Text, IBufferWriter<byte> bufferWriter)
+    {
+        ArgumentNullException.ThrowIfNull(textEncoder);
+        ArgumentNullException.ThrowIfNull(bufferWriter);
+        long length = 0;
+        do
+        {
+            var status = textEncoder.EncodeUtf8(utf8Text, bufferWriter.GetSpan(textEncoder.MaxOutputCharactersPerInputCharacter), out var consumed, out var written);
+            if (written > 0)
+            {
+                bufferWriter.Advance(written);
+                length += written;
+                utf8Text = utf8Text.Slice(consumed);
+
+                if (status == OperationStatus.Done) break;
+            }
+            else if (status == OperationStatus.DestinationTooSmall)
+            {
+                throw new InvalidOperationException("DestinationTooSmall");
+            }
+        } while (true);
+
+        Debug.Assert(utf8Text.IsEmpty);
+
+        return length;
+    }
+}
+#endif
